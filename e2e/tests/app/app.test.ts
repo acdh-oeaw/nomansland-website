232df108042682f1e5ba706bc64b995578e66fc6
Stefan Probst <stefanprobst@posteo.de>
2024-06-09 20:32:56 +0200

    chore: clean up

--- e2e/tests/app/app.test.ts
+++ e2e/tests/app/app.test.ts
@@ -65,17 +65,17 @@ test.describe("app", () => {
 		}
 	});
 
-	test("should serve a webmanifest", async ({ request }) => {
+	test("should serve a webmanifest", async ({ createI18n, request }) => {
 		const response = await request.get("/manifest.webmanifest");
 		const body = await response.body();
 
-		// TODO: use toMatchSnapshot
+		const i18n = await createI18n();
+
 		expect(body.toString()).toEqual(
 			JSON.stringify({
-				name: "Nomad's Manuscripts Landscape",
-				short_name: "NoMansLand",
-				description:
-					"Investigating Literary Evidence for Transculturation in Medieval Iran and Central Asia in the 13th – 15th centuries.",
+				name: i18n.t("metadata.title"),
+				short_name: i18n.t("metadata.shortTitle"),
+				description: i18n.t("metadata.description"),
 				icons: [
 					{ src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
 					{ src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
--- e2e/tests/app/metadata.test.ts
+++ e2e/tests/app/metadata.test.ts
@@ -1,5 +1,7 @@
 import { createUrl } from "@acdh-oeaw/lib";
 
+import { defaultLocale } from "@/config/i18n.config";
+import { escape } from "@/lib/safe-json-ld-replacer";
 import { expect, test } from "~/e2e/lib/test";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -13,9 +15,12 @@ test("should set a canonical url", async ({ createIndexPage }) => {
 	await expect(canonicalUrl).toHaveAttribute("href", String(createUrl({ baseUrl, pathname: "/" })));
 });
 
-test("should set document title on not-found page", async ({ page }) => {
+test("should set document title on not-found page", async ({ createI18n, page }) => {
+	const i18n = await createI18n();
 	await page.goto("/unknown/");
-	await expect(page).toHaveTitle("Page not found | Nomad's Manuscripts Landscape");
+	await expect(page).toHaveTitle(
+		[i18n.t("NotFoundPage.meta.title"), i18n.t("metadata.title")].join(" | "),
+	);
 });
 
 test("should disallow indexing of not-found page", async ({ page }) => {
@@ -25,12 +30,15 @@ test("should disallow indexing of not-found page", async ({ page }) => {
 	await expect(ogTitle).toHaveAttribute("content", "noindex");
 });
 
-// eslint-disable-next-line playwright/no-skipped-test
-test.skip("should set page metadata", async ({ createIndexPage }) => {
-	const { indexPage } = await createIndexPage();
+test("should set page metadata", async ({ createIndexPage }) => {
+	const { indexPage, i18n } = await createIndexPage();
 	await indexPage.goto();
 
 	const { page } = indexPage;
+
+	expect(i18n.t("metadata.title")).toBeTruthy();
+	expect(i18n.t("metadata.shortTitle")).toBeTruthy();
+	expect(i18n.t("metadata.description")).toBeTruthy();
 
 	const ogType = page.locator('meta[property="og:type"]');
 	await expect(ogType).toHaveAttribute("content", "website");
@@ -39,44 +47,39 @@ test.skip("should set page metadata", async ({ createIndexPage }) => {
 	await expect(twCard).toHaveAttribute("content", "summary_large_image");
 
 	const twCreator = page.locator('meta[name="twitter:creator"]');
-	await expect(twCreator).toHaveAttribute("content", "@NoMansLand_OEAW");
+	await expect(twCreator).toHaveAttribute("content", i18n.t("metadata.twitter"));
 
 	const twSite = page.locator('meta[name="twitter:site"]');
-	await expect(twSite).toHaveAttribute("content", "@NoMansLand_OEAW");
+	await expect(twSite).toHaveAttribute("content", i18n.t("metadata.twitter"));
 
 	// const googleSiteVerification = page.locator('meta[name="google-site-verification"]');
 	// await expect(googleSiteVerification).toHaveAttribute("content", "");
 
-	await expect(page).toHaveTitle("Home | Nomad's Manuscripts Landscape");
+	await expect(page).toHaveTitle(
+		[i18n.t("IndexPage.meta.title"), i18n.t("metadata.title")].join(" | "),
+	);
 
 	const metaDescription = page.locator('meta[name="description"]');
-	await expect(metaDescription).toHaveAttribute(
-		"content",
-		"Investigating Literary Evidence for Transculturation in Medieval Iran and Central Asia in the 13th – 15th centuries.",
-	);
+	await expect(metaDescription).toHaveAttribute("content", i18n.t("metadata.description"));
 
 	const ogTitle = page.locator('meta[property="og:title"]');
-	await expect(ogTitle).toHaveAttribute("content", "Home");
+	await expect(ogTitle).toHaveAttribute("content", i18n.t("IndexPage.meta.title"));
 
 	const ogDescription = page.locator('meta[property="og:description"]');
-	await expect(ogDescription).toHaveAttribute(
-		"content",
-		"Investigating Literary Evidence for Transculturation in Medieval Iran and Central Asia in the 13th – 15th centuries.",
-	);
+	await expect(ogDescription).toHaveAttribute("content", i18n.t("metadata.description"));
 
 	const ogSiteName = page.locator('meta[property="og:site_name"]');
-	await expect(ogSiteName).toHaveAttribute("content", "Nomad's Manuscripts Landscape");
+	await expect(ogSiteName).toHaveAttribute("content", i18n.t("metadata.title"));
 
 	const ogUrl = page.locator('meta[property="og:url"]');
 	await expect(ogUrl).toHaveAttribute("content", String(createUrl({ baseUrl, pathname: "/" })));
 
 	const ogLocale = page.locator('meta[property="og:locale"]');
-	await expect(ogLocale).toHaveAttribute("content", "en");
+	await expect(ogLocale).toHaveAttribute("content", defaultLocale);
 });
 
-// eslint-disable-next-line playwright/no-skipped-test
-test.skip("should add json+ld metadata", async ({ createIndexPage }) => {
-	const { indexPage } = await createIndexPage();
+test("should add json+ld metadata", async ({ createIndexPage }) => {
+	const { indexPage, i18n } = await createIndexPage();
 	await indexPage.goto();
 
 	const metadata = await indexPage.page.locator('script[type="application/ld+json"]').textContent();
@@ -85,9 +88,8 @@ test.skip("should add json+ld metadata", async ({ createIndexPage }) => {
 		JSON.stringify({
 			"@context": "https://schema.org",
 			"@type": "WebSite",
-			name: "Nomad&apos;s Manuscripts Landscape",
-			description:
-				"Investigating Literary Evidence for Transculturation in Medieval Iran and Central Asia in the 13th – 15th centuries.",
+			name: escape(i18n.t("metadata.title")),
+			description: escape(i18n.t("metadata.description")),
 		}),
 	);
 });
@@ -115,5 +117,5 @@ test("should set `lang` attribute on `html` element", async ({ createIndexPage }
 	const { indexPage } = await createIndexPage();
 	await indexPage.goto();
 
-	await expect(indexPage.page.locator("html")).toHaveAttribute("lang", "en");
+	await expect(indexPage.page.locator("html")).toHaveAttribute("lang", defaultLocale);
 });
