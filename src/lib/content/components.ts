09a8a811c7bb384e3832404656427a0b05177472
Stefan Probst <stefanprobst@posteo.de>
2024-08-17 08:41:43 +0200

    chore: update deps

--- src/lib/content/components.ts
+++ src/lib/content/components.ts
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import type { MDXComponents } from "mdx/types";
-
 import Download from "@/components/content/download.astro";
 import Figure from "@/components/content/figure.astro";
 import Img from "@/components/content/img.astro";
@@ -9,15 +7,19 @@ import PartnerInstitution from "@/components/content/partner-institution.astro";
 import TableOfContents from "@/components/content/table-of-contents.astro";
 import NavLink from "@/components/navigation/nav-link.astro";
 
-export function useMDXComponents(): MDXComponents {
-	return {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		a: NavLink as any,
-		Download,
-		Figure,
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		img: Img as any,
-		PartnerInstitution,
-		TableOfContents,
-	};
+const components = {
+	a: NavLink as any,
+	Download,
+	Figure,
+	img: Img,
+	PartnerInstitution,
+	TableOfContents,
+};
+
+declare global {
+	type MDXProvidedComponents = typeof components;
+}
+
+export function useMDXComponents(): MDXProvidedComponents {
+	return components;
 }
